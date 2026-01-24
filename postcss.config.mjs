835799609dbc5cb13c4ff1a2ac10dc8911987f7a
KENSUKEARAI <arakeeen525@gmail.com>
2026-01-25 01:32:05 +0900

    fix: Tailwind CSS v4へ完全移行

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {}, // ★ここを 'tailwindcss' から変更しました
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
 };
 
